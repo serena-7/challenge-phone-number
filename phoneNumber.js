c6a0c3d7fc572bc2511346072f9fb9f03e2a9726
Serena Sorensen <serena.sorensen7@gmail.com>
2022-04-19 09:24:26 -0600

    challenge complete

---
+++ phoneNumber.js
@@ -0,0 +1,3 @@
+const createPhoneNumber = arr => `(${arr.slice(0,3).join('')}) ${arr.slice(3,6).join('')}-${arr.slice(6,arr.length).join('')}`;
+
+console.log(createPhoneNumber([1,2,3,4,5,6,7,8,9,0]));
